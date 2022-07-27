f461dfd75c0c6a041cad99b2c702f59a21b7116f
carlos_palomino <carlospalominoes@gmail.com>
2022-07-27 17:00:39 -0500

    conectando a db

--- db/config.js
+++ db/config.js
@@ -9,7 +9,7 @@ const dbConnection = async()=>{
         await mongoose.connect(process.env.DB_CNN,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            // useCreateIndex: true
+           
         });
         
         console.log('DB Online');
